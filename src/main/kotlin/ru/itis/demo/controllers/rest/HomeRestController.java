01f97c1f2d56165445c12d750731a0876f257261
Ilya Azin <martis.azin@gmail.com>
2020-03-06 13:14:42 +0300

    attempt to add graphql

---
+++ src/main/kotlin/ru/itis/demo/controllers/rest/HomeRestController.java
@@ -0,0 +1,26 @@
+package ru.itis.demo.controllers.rest;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+public class HomeRestController {
+
+    @GetMapping("/home")
+    public Map<String, String> home() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        map.put("foo", "bar");
+        map.put("aa", "bb");
+        map.put("user", "Martis");
+        return map;
+    }
+}
--- src/main/kotlin/ru/itis/demo/controllers/rest/TechRestController.java
+++ src/main/kotlin/ru/itis/demo/controllers/rest/TechRestController.java
@@ -1,6 +1,7 @@
 package ru.itis.demo.controllers.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/tech")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TechRestController {
     @Autowired
     public TechService techService;
